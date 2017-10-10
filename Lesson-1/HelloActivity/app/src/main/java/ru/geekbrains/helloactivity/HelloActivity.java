11c24065087f642b0374fd457352d86726cf3a2b
DwarfPorter <mrvlmor@gmail.com>
2017-10-10 21:15:59 +0300

    Smart greetings (Good afternoon, Good night...)

--- Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/HelloActivity.java
+++ Lesson-1/HelloActivity/app/src/main/java/ru/geekbrains/helloactivity/HelloActivity.java
@@ -1,13 +1,32 @@
+
 package ru.geekbrains.helloactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
 
-public class HelloActivity extends AppCompatActivity {
-
+public class HelloActivity extends AppCompatActivity {  // Наследуем класс Activity
+    // или его потомка
+    // Этот метод вызывается при создании Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        TextView greeting = (TextView) findViewById(R.id.greeting); // Получить элемент
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY); // час сейчас
+        if (5 <= currentHour && currentHour < 12 ){         // Если утро
+            greeting.setText("Good morning GeekBrains!");
+        }
+        else if (12 <= currentHour && currentHour < 6){     // Если день
+            greeting.setText("Good afternoon GeekBrains!");
+        }
+        else if (6 <= currentHour && currentHour < 9){      // Если вечер
+            greeting.setText("Good evening GeekBrains!");
+        }
+        else {
+            greeting.setText("Good night GeekBrains!");     // Если поздний вечер или ночь
+        }
     }
 }
