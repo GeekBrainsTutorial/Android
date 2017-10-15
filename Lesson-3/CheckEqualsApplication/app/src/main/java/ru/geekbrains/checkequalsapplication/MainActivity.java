43ffc9fd0eefe20fa8b5af5c0c40a426dbdd0bd5
DwarfPorter <mrvlmor@gmail.com>
2017-10-15 20:08:50 +0300

    CheckEquals: change phrase.

--- Lesson-3/CheckEqualsApplication/app/src/main/java/ru/geekbrains/checkequalsapplication/MainActivity.java
+++ Lesson-3/CheckEqualsApplication/app/src/main/java/ru/geekbrains/checkequalsapplication/MainActivity.java
@@ -29,12 +29,10 @@ public class MainActivity extends AppCompatActivity {
                     textView.setText("Равно!");
                 }
                 else{
-                    textView.setText("Равно!");
+                    textView.setText("Не равно!");
                 }
 
             }
         });
     }
-
-
 }
