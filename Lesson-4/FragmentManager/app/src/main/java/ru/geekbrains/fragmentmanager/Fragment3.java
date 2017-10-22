148f03a2ed6eb04e041baf6bec2757c4b09feefb
DwarfPorter <mrvlmor@gmail.com>
2017-10-22 17:52:10 +0300

    FragmentManager: control fragments from code

---
+++ Lesson-4/FragmentManager/app/src/main/java/ru/geekbrains/fragmentmanager/Fragment3.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.fragmentmanager;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_3, container, false);
+    }
+}
