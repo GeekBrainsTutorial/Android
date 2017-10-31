160817f942bb8a9b98814ccf91648ee523286ea8
DwarfPorter <mrvlmor@gmail.com>
2017-10-31 22:46:38 +0300

    CityList: make list from inflater

--- Lesson-6/CityList/app/src/main/java/ru/geekbrains/citylist/MainActivity.java
+++ Lesson-6/CityList/app/src/main/java/ru/geekbrains/citylist/MainActivity.java
@@ -1,8 +1,9 @@
 package ru.geekbrains.citylist;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -19,18 +20,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout list = findViewById(R.id.layout);
+
+        // При помощи этого объекта будем доставать элементы, спрятанные в item.xml
+        LayoutInflater ltInflater = getLayoutInflater();
         final Activity that = this;
-        for(String city : cities){  // бежим  цикле и создаем на каждый город по элементу
-            TextView cityView = new TextView(this);
+        for(String city : cities){  // бежим в цикле и создаем на каждый город по элементу
+            // Достаем элемент из item.xml
+            View item = ltInflater.inflate(R.layout.item, list, false);
+            // Находим в этом элементе TextView
+            TextView cityView = item.findViewById(R.id.textView);
+            // Меняем текст
             cityView.setText(city);
-            cityView.setTextSize(42);
             cityView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(that, String.format("Выбран город - %s", ((TextView)v).getText()), Toast.LENGTH_SHORT).show();
                 }
             });
-            list.addView(cityView);
+            list.addView(item);
         }
     }
 }
