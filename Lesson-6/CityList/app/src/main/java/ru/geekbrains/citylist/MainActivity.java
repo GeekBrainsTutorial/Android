3cf3de9c1e6b412a5c58aa336a8a90ebc8777b3e
DwarfPorter <mrvlmor@gmail.com>
2017-10-31 22:56:28 +0300

    CityList: make ListView

--- Lesson-6/CityList/app/src/main/java/ru/geekbrains/citylist/MainActivity.java
+++ Lesson-6/CityList/app/src/main/java/ru/geekbrains/citylist/MainActivity.java
@@ -3,9 +3,10 @@ package ru.geekbrains.citylist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,25 +20,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout list = findViewById(R.id.layout);
+        // Это список
+        ListView listView = findViewById(R.id.listCities);
 
-        // При помощи этого объекта будем доставать элементы, спрятанные в item.xml
-        LayoutInflater ltInflater = getLayoutInflater();
+        // Создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, cities);
+
+        // устанавливаем адаптер в список
+        listView.setAdapter(adapter);
+
         final Activity that = this;
-        for(String city : cities){  // бежим в цикле и создаем на каждый город по элементу
-            // Достаем элемент из item.xml
-            View item = ltInflater.inflate(R.layout.item, list, false);
-            // Находим в этом элементе TextView
-            TextView cityView = item.findViewById(R.id.textView);
-            // Меняем текст
-            cityView.setText(city);
-            cityView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(that, String.format("Выбран город - %s", ((TextView)v).getText()), Toast.LENGTH_SHORT).show();
-                }
-            });
-            list.addView(item);
-        }
+        // обработка нажатий
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(that, String.format("Выбран город - %s", ((TextView)v).getText()), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
