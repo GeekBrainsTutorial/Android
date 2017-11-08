423d399486941f42760e1057844a02a41dc91722
DwarfPorter <mrvlmor@gmail.com>
2017-11-08 23:20:32 +0300

    OkHttp: Asynchronous request http. Attention, the application has error!

--- Lesson-7/OkHttp/app/src/main/java/ru/geekbrains/okhttp/OkHttpRequester.java
+++ Lesson-7/OkHttp/app/src/main/java/ru/geekbrains/okhttp/OkHttpRequester.java
@@ -1,11 +1,13 @@
 package ru.geekbrains.okhttp;
 
-import android.os.AsyncTask;
-import android.util.Log;
 import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
+import static junit.framework.Assert.fail;
 
 // Здесь будем вызывать запросы
 public class OkHttpRequester {
@@ -17,49 +19,30 @@ public class OkHttpRequester {
 
     // Синхронный запрос страницы
     public void run(String url) {
-        Requester requester = new Requester(listener);
-        requester.execute(url);
+        OkHttpClient client = new OkHttpClient();        // Клиент
+        Request.Builder builder = new Request.Builder(); // создадим строителя
+        builder.url(url);                                // укажем адрес сервера
+        Request request = builder.build();               // построим запрос
+
+        Call call = client.newCall(request);            // Ставим запрос в очередь
+        call.enqueue(new Callback() {
+
+            // Это срабатывает по приходу ответа от сервера
+            public void onResponse(Call call, Response response)
+                    throws IOException {
+                String answer = response.body().string();
+                listener.onCompleted(answer);               // вызовем наш метод обратного вызова
+            }
+
+            // При сбое
+            public void onFailure(Call call, IOException e) {
+                fail();
+            }
+        });
     }
 
     // интерфейс обратного вызова, метод onCompleted вызывается по окончании загрузки страницы
     public interface OnResponseCompleted {
         void onCompleted(String content);
-    }
-
-    // Асинхронная задача
-    static class Requester extends AsyncTask<String, Void, String>{
-        OnResponseCompleted listener;   // слушатель
-
-        Requester(OnResponseCompleted listener){
-            this.listener = listener;
-        }
-
-        // Это выполняется в фоновом потоке
-        @Override
-        protected String doInBackground(String... url) {
-            return getContent(url[0]);
-        }
-
-        // Это выполняется по завершении работы в потоке UI
-        @Override
-        protected void onPostExecute(String content){
-            listener.onCompleted(content);      // Вызовем обратный вызов
-        }
-
-        // получение данных при отправке запроса
-        private String getContent(String url){
-            OkHttpClient client = new OkHttpClient();        // Клиент
-            Request.Builder builder = new Request.Builder(); // создадим строителя
-            builder.url(url);                                // укажем адрес сервера
-            Request request = builder.build();               // построим запрос
-
-            try (Response response = client.newCall(request).execute()) { // выполним запрос
-                return response.body().string();                          // возмем ответ сервера
-            } catch (IOException e) {
-                Log.e("OkHttp", e.getMessage(), e);     // при ошибке - логируем
-                e.printStackTrace();
-            }
-            return "";
-        }
     }
 }
