962a3073a16c9b51c3a007b7c06d013077a1e74c
DwarfPorter <mrvlmor@gmail.com>
2017-12-19 00:45:55 +0300

    Broadcast:  receive only when active

--- Android-2/Lesson-7/BroadcastReceiver/app/src/main/java/ru/geekbrains/broadcastreceiver/MainActivity.java
+++ Android-2/Lesson-7/BroadcastReceiver/app/src/main/java/ru/geekbrains/broadcastreceiver/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.broadcastreceiver;
 
 import android.app.Activity;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerReceiver(new TestBroadcastReceiver(), new IntentFilter("ru.geekbrains.action.TestReceiver"));
     }
 }
