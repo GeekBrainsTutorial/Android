85e3928b92780e621f6f926578cab6c2b89ee62e
DwarfPorter <mrvlmor@gmail.com>
2017-12-27 00:05:48 +0300

    GMap: tracker

--- Android-2/Lesson-8/GMap/app/src/main/java/ru/geekbrains/gmap/MapsActivity.java
+++ Android-2/Lesson-8/GMap/app/src/main/java/ru/geekbrains/gmap/MapsActivity.java
@@ -1,8 +1,16 @@
 package ru.geekbrains.gmap;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +18,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap mMap;
+    private static final int PERMISSION_REQUEST_CODE = 10;
+    private double myLatitude = 0;
+    private double myLongitude = 0;
+    private LatLng myLocation = null;
+    private MarkerOptions myMarker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +35,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
+
+    }
 
     /**
      * Manipulates the map once available.
@@ -34,13 +48,91 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);    // Отобразить кнопку поиска своего положения
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+                return true;
+            }
+        });
+        requestPermission();
     }
+
+    private void requestPermission() {
+        // Проверим на пермиссии, и если их нет, запросим у пользователя
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // запросим координаты
+            requestLocation();
+        } else {
+            // пермиссии нет, будем запрашивать у пользователя
+            requestLocationPermissions();
+        }
+    }
+
+    // Здесь будем обрабатывать координаты
+    @SuppressLint("MissingPermission")
+    private void requestLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String provider = LocationManager.GPS_PROVIDER;
+        // Будем получать геоположение через каждые 10 секунд или каждые 10 метров
+        locationManager.requestLocationUpdates(provider, 10000, 10, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                myLatitude = location.getLatitude();  // Широта
+                myLongitude = location.getLongitude();// Долгота
+                myLocation = new LatLng(myLatitude, myLongitude);
+                if (myMarker == null) { // Если маркера еще нет, значит инициализируем его
+                    myMarker = new MarkerOptions().visible(false).icon(null).position(myLocation);
+                    mMap.addMarker(myMarker);
+                } else {
+                    myMarker.position(myLocation);  // Маркер есть, просто сменим его положение
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        });
+    }
+
+    // Запрос пермиссии для геолокации
+    private void requestLocationPermissions() {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
+            // Запросим эти две пермиссии у пользователя
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    // Это результат запроса у пользователя пермиссии
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {   // Это та самая пермиссия, что мы запрашивали?
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Все препоны пройдены и пермиссия дана
+                requestLocation();
+            }
+        }
+    }
+
 }
