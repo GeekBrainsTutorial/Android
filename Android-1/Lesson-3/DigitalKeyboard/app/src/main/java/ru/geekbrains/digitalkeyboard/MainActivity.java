ed6155775b2004dae8c2e0e520315601c756aa5f
DwarfPorter <mrvlmor@gmail.com>
2019-04-09 21:06:27 +0300

    Lesson3: Digital Keyboard. FrameLayout

---
+++ Android-1/Lesson-3/DigitalKeyboard/app/src/main/java/ru/geekbrains/digitalkeyboard/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.digitalkeyboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_framelayout);
+    }
+}
