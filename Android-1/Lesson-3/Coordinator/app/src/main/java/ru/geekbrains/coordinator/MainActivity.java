f91b5df0ea7f1c583c54b4b833f6740fa7cfa0ec
DwarfPorter <mrvlmor@gmail.com>
2019-04-10 22:58:08 +0300

    Lesson3. Coordinator. Scrolling.

--- Android-1/Lesson-3/Coordinator/app/src/main/java/ru/geekbrains/coordinator/MainActivity.java
+++ Android-1/Lesson-3/Coordinator/app/src/main/java/ru/geekbrains/coordinator/MainActivity.java
@@ -8,8 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TEXT_CONTENT = "В 2014 году на конференции был представлен новый подход к дизайну приложений. " +
+            "Это попытка сделать единообразный интерфейс для всех приложений Google, " +
+            "неважно где они работают на телефоне, планшете или компьютере. " +
+            "А также для всех Андроид приложений. Данный стиль основан на размещении плоской бумаги на экране. " +
+            "Бумага тонкая, плоская, но расположенная в трехмерном пространстве, с тенями, с движением. " +
+            "Такую бумагу называют квантумной, или цифровой. Если происходит анимация, " +
+            "то она и показывает пользователю, что происходит. Однако чрезмерная анимация не нужна, " +
+            "никому не интересно ждать пару секунд, пока окно с сообщением налетается по экрану.\n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +29,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Обработка нажатия на плавающую кнопку
+        // покажем на экране длинный текст для скроллинга
+        TextView content = findViewById(R.id.content);
+        content.setText(TEXT_CONTENT);
+
         FloatingActionButton fab = findViewById(R.id.fab);
+        // Обработка нажатия на плавающую кнопку
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Здесь вылетит Snackbar
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Вы нажали на плавающую кнопку", Snackbar.LENGTH_LONG).show();
             }
         });
     }
