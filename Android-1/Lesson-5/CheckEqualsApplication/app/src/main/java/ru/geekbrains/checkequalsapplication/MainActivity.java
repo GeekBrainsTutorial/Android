2b4e93f355da209d1e88639cd8bee9096b49ff71
DwarfPorter <mrvlmor@gmail.com>
2019-04-27 22:54:49 +0300

    CheckEquals: change phrase.

--- Android-1/Lesson-5/CheckEqualsApplication/app/src/main/java/ru/geekbrains/checkequalsapplication/MainActivity.java
+++ Android-1/Lesson-5/CheckEqualsApplication/app/src/main/java/ru/geekbrains/checkequalsapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
                     textView.setText("Равно!");
                 }
                 else{
-                    textView.setText("Равно!");
+                    textView.setText("Не равно!");
                 }
 
             }
