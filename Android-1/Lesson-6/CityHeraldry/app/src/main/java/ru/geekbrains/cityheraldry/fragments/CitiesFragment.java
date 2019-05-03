a0e63b00207cf49d16457fa8ab45f4dc82f2b74b
DwarfPorter <mrvlmor@gmail.com>
2019-05-03 19:29:59 +0300

    CityHeraldry: Complex parameter of fragment

--- Android-1/Lesson-6/CityHeraldry/app/src/main/java/ru/geekbrains/cityheraldry/fragments/CitiesFragment.java
+++ Android-1/Lesson-6/CityHeraldry/app/src/main/java/ru/geekbrains/cityheraldry/fragments/CitiesFragment.java
@@ -14,12 +14,17 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import ru.geekbrains.cityheraldry.CoatOfArmsActivity;
+import ru.geekbrains.cityheraldry.Parcel;
 import ru.geekbrains.cityheraldry.R;
+
+import static ru.geekbrains.cityheraldry.fragments.CoatOfArmsFragment.PARCEL;
 
 // Фрагмент выбора города из списка
 public class CitiesFragment extends Fragment {
     boolean isExistCoatOfArms;  // Можно ли расположить рядом фрагмент с гербом
-    int currentPosition = 0;    // Текущая позиция (выбранный город)
+
+    //+ Меняем текущую позицию на объект Parcel
+    Parcel currentParcel;       // Текущая посылка (номер города и название)
 
     // При создании фрагмента укажем его макет
     @Override
@@ -44,22 +49,28 @@ public class CitiesFragment extends Fragment {
                 == Configuration.ORIENTATION_LANDSCAPE;
 
         // Если это не первое создание, то восстановим текущую позицию
+        //+ Здесь также востанавливаем Parcel
         if (savedInstanceState != null) {
             // Восстановление текущей позиции.
-            currentPosition = savedInstanceState.getInt("CurrentCity", 0);
+            currentParcel = (Parcel) savedInstanceState.getSerializable("CurrentCity");
+        }
+        else {
+            //+ Если востановить не удалось, то сделаем объект с первым индексом
+            currentParcel = new Parcel(0, getResources().getStringArray(R.array.cities)[0]);
         }
 
         // Если можно нарисовать рядом герб, то сделаем это
         if (isExistCoatOfArms) {
-            showCoatOfArms();
+            showCoatOfArms(currentParcel);
         }
     }
 
     // Сохраним текущую позицию (вызывается перед выходом из фрагмента)
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt("CurrentCity", currentPosition);
         super.onSaveInstanceState(outState);
+        //+ Также меняем текущую позицию на Parcel
+        outState.putSerializable("CurrentCity", currentParcel);
     }
 
     // создаем список городов на экране из массива в ресурсах
@@ -81,8 +92,9 @@ public class CitiesFragment extends Fragment {
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    currentPosition = fi;
-                    showCoatOfArms();
+                    //+ Теперь опираемся на Parcel, а не на текущую позицию
+                    currentParcel = new Parcel(fi, getResources().getStringArray(R.array.cities)[fi]);
+                    showCoatOfArms(currentParcel);
                 }
             });
         }
@@ -90,15 +102,16 @@ public class CitiesFragment extends Fragment {
 
     // Показать герб. Ecли возможно, то показать рядом со списком,
     // если нет, то открыть вторую activity
-    private void showCoatOfArms() {
+    private void showCoatOfArms(Parcel parcel) {
         if (isExistCoatOfArms) {
             // Проверим, что фрагмент с гербом существует в activity
             CoatOfArmsFragment detail = (CoatOfArmsFragment)
                     getFragmentManager().findFragmentById(R.id.coat_of_arms);
             // Если есть необходимость, то выведем герб
-            if (detail == null || detail.getIndex() != currentPosition) {
+            //+ Здесь также применяем Parcel
+            if (detail == null || detail.getParcel().getImageIndex() != parcel.getImageIndex()) {
                 // Создаем новый фрагмент с текущей позицией для вывода герба
-                detail = CoatOfArmsFragment.create(currentPosition);
+                detail = CoatOfArmsFragment.create(parcel);
 
                 // Выполняем транзакцию по замене фрагмента
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
@@ -110,8 +123,8 @@ public class CitiesFragment extends Fragment {
             // Если нельзя вывести герб рядом, откроем вторую activity
             Intent intent = new Intent();
             intent.setClass(getActivity(), CoatOfArmsActivity.class);
-            // и передадим туда параметры
-            intent.putExtra("index", currentPosition);
+            //+ и передадим туда Parcel
+            intent.putExtra(PARCEL, parcel);
             startActivity(intent);
         }
     }
--- Android-1/Lesson-6/CityHeraldry/app/src/main/java/ru/geekbrains/cityheraldry/fragments/CoatOfArmsFragment.java
+++ Android-1/Lesson-6/CityHeraldry/app/src/main/java/ru/geekbrains/cityheraldry/fragments/CoatOfArmsFragment.java
@@ -7,39 +7,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import ru.geekbrains.cityheraldry.Parcel;
 import ru.geekbrains.cityheraldry.R;
 
 public class CoatOfArmsFragment extends Fragment {
+    public static final String PARCEL = "parcel";
 
     // Фабричный метод создания фрагмента
     // Фрагменты рекомендуется создавать через фабричные методы.
-    public static CoatOfArmsFragment create(int index) {
+    public static CoatOfArmsFragment create(Parcel parcel) {
         CoatOfArmsFragment f = new CoatOfArmsFragment();    // создание
 
         // Передача параметра
         Bundle args = new Bundle();
-        args.putInt("index", index);
+        args.putSerializable(PARCEL, parcel);
         f.setArguments(args);
         return f;
     }
 
-    // Получить индекс из списка (фактически из параметра)
-    public int getIndex() {
-        int index = getArguments().getInt("index", 0);
-        return index;
+    // Получить посылку из параметра
+    public Parcel getParcel() {
+        Parcel parcel = (Parcel) getArguments().getSerializable(PARCEL);
+        return parcel;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Определить какой герб надо показать, и показать его
-        ImageView coatOfArms = new ImageView(getContext());
+        View layout = inflater.inflate(R.layout.fragment_coatofarms, container, false);
+
+        ImageView coatOfArms = layout.findViewById(R.id.imageView);
+        TextView cityNameView = layout.findViewById(R.id.textView);
 
         // Получить из ресурсов массив указателей на изображения гербов
         TypedArray imgs = getResources().obtainTypedArray(R.array.coatofarms_imgs);
+        Parcel parcel = getParcel();
+
         // Выбрать по индексу подходящий
-        coatOfArms.setImageResource(imgs.getResourceId(getIndex(), -1));
-        return coatOfArms;     // Вместо макета используем сразу картинку
+        coatOfArms.setImageResource(imgs.getResourceId(parcel.getImageIndex(), -1));
+        cityNameView.setText(parcel.getCityName());
+        return layout;
     }
 }
